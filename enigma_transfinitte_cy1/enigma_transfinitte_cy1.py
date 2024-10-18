9ffe5cb2241480e20e3bd86c32a92bc792d2fdf2
jayanthh71 <jayanth37069@gmail.com>
2024-10-19 02:31:49 +0530

    Refactor code structure and update UI in enigma_transfinitte_cy1.py

--- enigma_transfinitte_cy1/enigma_transfinitte_cy1.py
+++ enigma_transfinitte_cy1/enigma_transfinitte_cy1.py
@@ -1,37 +1,76 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
-
 from rxconfig import config
 
 
-class State(rx.State):
-    """The app state."""
+class GitHubState(rx.State):
+    link: str = ""
+    is_link_valid: bool = False
 
-    ...
+    def validate_link(self) -> None:
+        self.is_link_valid = True
+
+
+class InputState(rx.State):
+    input_text: str = ""
+    display_language: bool = True
+    current_language: str = "None"
+
+    def change_language(self) -> None:
+        self.display_language = True
 
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading("Code Safety Verification Tool", size="9"),
+            rx.divider(),
             rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+                "This tool is used for checking the safety of code snippets with the help of a LLM",
+                size="4",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
+            rx.hstack(
+                rx.input(
+                    placeholder="Enter GitHub Repository URL",
+                    width="395px",
+                    height="35px",
+                    value=GitHubState.link,
+                    on_change=GitHubState.is_link_valid,
+                ),
+                rx.button(
+                    "Import",
+                    width="95px",
+                    height="35px",
+                    disabled=GitHubState.is_link_valid,
+                ),
             ),
+            rx.text_area(
+                placeholder="Paste code here",
+                width="500px",
+                height="400px",
+            ),
+            rx.cond(
+                InputState.display_language,
+                rx.hstack(
+                    rx.badge(
+                        InputState.current_language,
+                        size="3",
+                        width="95px",
+                        height="35px",
+                    ),
+                    rx.button(
+                        "Verify",
+                        width="95px",
+                        height="35px",
+                        disabled=bool(InputState.input_text),
+                    ),
+                ),
+            ),
+            align="center",
             spacing="5",
             justify="center",
             min_height="85vh",
         ),
-        rx.logo(),
     )
 
 

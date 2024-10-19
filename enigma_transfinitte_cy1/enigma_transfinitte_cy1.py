97ee4f895933c56d5c923ba468e0bde60c7a093c
jayanthh71 <jayanth37069@gmail.com>
2024-10-19 12:59:08 +0530

    Refactor code structure and update UI in enigma_transfinitte_cy1.py

--- enigma_transfinitte_cy1/enigma_transfinitte_cy1.py
+++ enigma_transfinitte_cy1/enigma_transfinitte_cy1.py
@@ -2,70 +2,99 @@ import reflex as rx
 from rxconfig import config
 
 
-class GitHubState(rx.State):
-    link: str = ""
-    is_link_valid: bool = False
+class ChatState(rx.State):
+    request: str
+    response: str
 
-    def validate_link(self) -> None:
-        self.is_link_valid = True
+    chat_history: list
+
+    def handle_request(self, data: dict):
+        self.request = data["prompt"]
+        self.response = "This is a test response"
 
 
-class InputState(rx.State):
-    input_text: str = ""
-    display_language: bool = True
-    current_language: str = "None"
+def textbubble(res_type: str) -> rx.Component:
+    return rx.flex(
+        rx.cond(
+            res_type == "user",
+            rx.text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                padding_inline="10px",
+                padding_block="5px",
+                background_color="#482c6c",
+                border_radius="12px",
+                max_width="70%",
+            ),
+            rx.text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                padding_inline="10px",
+                padding_block="5px",
+                background_color="#1f1f1f",
+                border_radius="12px",
+                max_width="70%",
+            ),
+        ),
+        direction="row-reverse" if res_type == "user" else "row",
+    )
 
-    def change_language(self) -> None:
-        self.display_language = True
+
+def chatbox() -> rx.Component:
+    return rx.flex(
+        rx.scroll_area(
+            rx.flex(
+                textbubble("user"),
+                textbubble("ai"),
+                textbubble("user"),
+                textbubble("ai"),
+                padding="15px",
+                direction="column",
+                spacing="4",
+            ),
+            type="hover",
+            scrollbars="vertical",
+        ),
+        rx.form.root(
+            rx.hstack(
+                rx.input(
+                    placeholder="Ask anything",
+                    id="prompt",
+                    radius="full",
+                    height="50px",
+                    class_name="relative w-full",
+                    padding_inline="15px",
+                ),
+                rx.icon_button(
+                    "arrow-up",
+                    radius="full",
+                    size="3",
+                    background_color="#482c6c",
+                ),
+                align="center",
+            ),
+            on_submit=ChatState.handle_request,
+            reset_on_submit=True,
+        ),
+        spacing="3",
+        direction="column",
+        padding="15px",
+        border="1px solid #43474e",
+        border_radius="12px",
+        height="600px",
+        width="1100px",
+    )
 
 
 def index() -> rx.Component:
     return rx.container(
-        rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Code Safety Verification Tool", size="9"),
+            rx.spacer(),
+            rx.heading("Code Safety Verification Chatbot", size="9"),
             rx.divider(),
             rx.text(
-                "This tool is used for checking the safety of code snippets with the help of a LLM",
+                "This Chatbot is used for checking the safety of code snippets with the help of a LLM",
                 size="4",
             ),
-            rx.hstack(
-                rx.input(
-                    placeholder="Enter GitHub Repository URL",
-                    width="395px",
-                    height="35px",
-                    value=GitHubState.link,
-                    on_change=GitHubState.is_link_valid,
-                ),
-                rx.button(
-                    "Import",
-                    width="95px",
-                    height="35px",
-                    disabled=GitHubState.is_link_valid,
-                ),
-            ),
-            rx.text_area(
-                placeholder="Paste code here",
-                width="500px",
-                height="400px",
-            ),
-            rx.cond(
-                InputState.display_language,
-                rx.hstack(
-                    rx.badge(
-                        InputState.current_language,
-                        size="3",
-                        width="95px",
-                        height="35px",
-                    ),
-                    rx.button(
-                        "Verify",
-                        width="95px",
-                        height="35px",
-                        disabled=bool(InputState.input_text),
-                    ),
-                ),
-            ),
+            chatbox(),
             align="center",
             spacing="5",
             justify="center",

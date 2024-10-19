d426f84db0e4ccc008f597448829f37c2cd75109
jayanthh71 <jayanth37069@gmail.com>
2024-10-19 15:29:36 +0530

    Finished main UI

--- enigma_transfinitte_cy1/enigma_transfinitte_cy1.py
+++ enigma_transfinitte_cy1/enigma_transfinitte_cy1.py
@@ -5,11 +5,23 @@ from rxconfig import config
 class ChatState(rx.State):
     request: str
     response: str
-    chat_history: list[tuple[str, str]]
+    chat_history: list[tuple[bool, str]] = []
+    is_user: bool = True
 
     def handle_submit(self, data: dict):
         self.request = data["prompt"]
-        self.response = "This is a test response"
+        if not self.request.strip():
+            return
+
+        self.chat_history.append((self.is_user, self.request))
+        self.response = self.get_response()
+        self.chat_history.append((not self.is_user, self.response))
+
+    def get_response(self):
+        return "This is a test response"
+
+    def clear_history(self):
+        self.chat_history = []
 
 
 def textbubble(is_user: bool, req: str = "", res: str = "") -> rx.Component:
@@ -41,8 +53,14 @@ def chatbox() -> rx.Component:
     return rx.flex(
         rx.scroll_area(
             rx.flex(
-                textbubble(True, req="Hello"),
-                textbubble(False, res="Hi"),
+                rx.foreach(
+                    ChatState.chat_history,
+                    lambda chat: rx.cond(
+                        chat[0],
+                        textbubble(True, req=chat[1]),
+                        textbubble(False, res=chat[1]),
+                    ),
+                ),
                 padding="15px",
                 direction="column",
                 spacing="4",
@@ -50,8 +68,8 @@ def chatbox() -> rx.Component:
             type="hover",
             scrollbars="vertical",
         ),
-        rx.form.root(
-            rx.hstack(
+        rx.hstack(
+            rx.form.root(
                 rx.input(
                     placeholder="Ask anything",
                     id="prompt",
@@ -60,16 +78,17 @@ def chatbox() -> rx.Component:
                     class_name="relative w-full",
                     padding_inline="15px",
                 ),
-                rx.icon_button(
-                    "arrow-up",
-                    radius="full",
-                    size="3",
-                    background_color="#482c6c",
-                ),
-                align="center",
+                on_submit=ChatState.handle_submit,
+                reset_on_submit=True,
             ),
-            on_submit=ChatState.handle_submit,
-            reset_on_submit=True,
+            rx.icon_button(
+                "trash",
+                radius="full",
+                size="3",
+                background_color="#482c6c",
+                on_click=ChatState.clear_history,
+            ),
+            align="center",
         ),
         spacing="3",
         direction="column",
